81659dac4cd3ae37becf6cd125f890e9d5e9bb3f
ShweSinHtay <shwesin@isgm2.com>
2020-12-04 12:42:22 +0630

    react

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './components/counter';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Counter />,
   document.getElementById('root')
 );
 
